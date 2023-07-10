68d8f44387e171f841a9d3b21ff08a388af35ac0
SpiritOfForest <dzahar@mail.ru>
2023-07-10 20:55:14 +0400

    lint: apply eslint rules

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,18 @@
 import { runHttpServer } from './http_server';
 import { runWebSocket } from './websocket';
 
-runHttpServer(8181).then((port) => {
+runHttpServer(8181)
+  .then((port) => {
     console.log(`Start static http server on the ${port} port!`);
-}).catch((err: Error) => {
+  })
+  .catch((err: Error) => {
     console.log(`The error occurred on the HTTP server: ${err}`);
-});
+  });
 
-runWebSocket(3000).then((port) => {
+runWebSocket(3000)
+  .then((port) => {
     console.log(`Start WebSocket server on the ${port} port!`);
-}).catch((err: Error) => {
+  })
+  .catch((err: Error) => {
     console.log(`The error occurred on the WebSocket server: ${err}`);
-});
+  });
